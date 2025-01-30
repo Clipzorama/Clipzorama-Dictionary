8882492006e7af8193adbe6f429246212349347a
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-01-29 23:59:55 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Beginning Dev Goes here
+
 const startDiv = document.querySelector(".content");
 const startBtn = document.getElementById("continue");
 const loader = document.querySelector(".honeycomb");
@@ -11,3 +13,17 @@ startBtn.addEventListener("click", () => {
     startDiv.classList.add("hidden");
     }, 3000)
 })
+
+
+// what we have for the API for now
+async function checkWord(word) {
+    let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    if (response.status == 404) {
+        alert("The word you entered is not found. Please check the spelling and try again.");
+    } else {
+        console.log(`Status Success: ${response.status} !`)
+        let data = await response.json();
+        console.log(data);
+    }
+}
+
